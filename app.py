724cc629d76fa99b883c7ca1bd3e0c5b4374c72d
Jesús Alfonseca <jesusalfonseca24@gmail.com>
2026-08-18 18:55:03 -0400

    Mensaje de los cambios

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/juego')
+def juego():
+    return render_template('juego/numero-secreto.html') 
+
 if __name__ == '__main__':
     app.run()
